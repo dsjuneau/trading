f289a224fe2ee4f50916a88d96d961110b0b5ad9
Scott Juneau <dsjuneau@gmail.com>
2020-12-12 12:26:56 -0600

    added dot-env

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const PORT = process.env.PORT || 5000;
+// const routes = require("./routes");
+const mongoose = require("mongoose");
+const app = express();
+require("dot-env");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// app.use(routes);
+
+app.get("*", (req, res) => {
+  res.sendFile("./client/build/index.html", { root: __dirname });
+});
+
+console.log(process.env.MONGO_URL);
+
+// mongoose
+//   .connect(process.env.MONGODB_URI, {
+//     useNewUrlParser: true,
+//   })
+//   .then(() => console.log("Database Connected"));
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
