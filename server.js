ce6b980bb9933e8e53c06d657655ab688c22553c
Scott Juneau <dsjuneau@gmail.com>
2020-12-12 12:39:11 -0600

    used correct dotenv

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 5000;
 // const routes = require("./routes");
 const mongoose = require("mongoose");
 const app = express();
-require("dot-env");
+require("dotenv").config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,7 +18,7 @@ app.get("*", (req, res) => {
   res.sendFile("./client/build/index.html", { root: __dirname });
 });
 
-console.log(process.env.MONGO_URL);
+console.log(process.env.key);
 
 // mongoose
 //   .connect(process.env.MONGODB_URI, {
